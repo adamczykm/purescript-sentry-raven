d58c300cfd07ce9165a035db38b61411f1ac4c0f
Tomasz Rybarczyk <paluho@gmail.com>
2018-04-18 20:11:43 +0200

    get/setContext test

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,25 @@
 /* global exports */
 "use strict";
-
 // module Sentry
 
 var Raven = require('raven');
 
-exports.ravenImpl = function(dsn) {
-  return new Raven.Client(dsn);
+exports.ravenImpl = function(dsn, ctx) {
+  var raven = new Raven.Client(dsn);
+  raven.setContext(ctx);
+  return raven;
 };
 
 exports.captureMessageImpl = function(raven, msg) {
   raven.captureMessage(msg);
+};
+
+exports.getContextImpl = function(raven) {
+  return raven.getContext();
+};
+
+exports.setContextImpl = function(raven, ctx) {
+  raven.setContext(ctx);
 };
 
 exports.inContextImpl = function(raven, eff) {
--- src/Main.purs
+++ src/Main.purs
@@ -8,49 +8,60 @@ import Control.Monad.Eff.Uncurried (EffFn2, runEffFn2, EffFn1, runEffFn1)
 import Debug.Trace (traceAnyA)
 import Unsafe.Coerce (unsafeCoerce)
 
-foreign import data Raven ∷ Type
+foreign import data Raven ∷ Type → Type
 foreign import data RAVEN ∷ Effect
 
 newtype Dsn = Dsn String
 
-foreign import ravenImpl ∷ ∀ eff. EffFn1 (raven ∷ RAVEN | eff) String Raven
+foreign import ravenImpl ∷ ∀ ctx eff. EffFn2 (raven ∷ RAVEN | eff) String ctx (Raven ctx)
 
-foreign import captureMessageImpl ∷ ∀ eff. EffFn2 (raven ∷ RAVEN | eff) Raven String Unit
+foreign import captureMessageImpl ∷ ∀ ctx eff. EffFn2 (raven ∷ RAVEN | eff) (Raven ctx) String Unit
 
-foreign import inContextImpl ∷ ∀ a eff. EffFn2 (raven ∷ RAVEN | eff) Raven (Eff (raven ∷ RAVEN | eff) a) a
+foreign import inContextImpl ∷ ∀ a ctx eff. EffFn2 (raven ∷ RAVEN | eff) (Raven ctx) (Eff (raven ∷ RAVEN | eff) a) a
 
-raven ∷ ∀ eff. Dsn → Eff (raven ∷ RAVEN | eff) Raven
-raven (Dsn s) = runEffFn1 ravenImpl s
+foreign import getContextImpl ∷ ∀ ctx eff. EffFn1 (raven ∷ RAVEN | eff) (Raven ctx) ctx
 
-captureMessage ∷ ∀ eff. Raven → String → Eff (raven ∷ RAVEN | eff) Unit
+foreign import setContextImpl ∷ ∀ ctx eff. EffFn2 (raven ∷ RAVEN | eff) (Raven ctx) ctx Unit
+
+raven ∷ ∀ ctx eff. Dsn → ctx → Eff (raven ∷ RAVEN | eff) (Raven ctx)
+raven (Dsn s) ctx = runEffFn2 ravenImpl s ctx
+
+captureMessage ∷ ∀ ctx eff. Raven ctx → String → Eff (raven ∷ RAVEN | eff) Unit
 captureMessage r msg = runEffFn2 captureMessageImpl r msg
+
+getContext ∷ ∀ ctx eff. Raven ctx → Eff (raven ∷ RAVEN | eff) ctx
+getContext r = runEffFn1 getContextImpl r
+
+setContext ∷ ∀ ctx eff. Raven ctx → ctx → Eff (raven ∷ RAVEN | eff) Unit
+setContext r ctx = runEffFn2 setContextImpl r ctx
 
 inContext ∷ ∀ a eff. Raven → Eff (raven ∷ RAVEN | eff) a → Eff (raven ∷ RAVEN | eff) a
 inContext r eff = runEffFn2 inContextImpl r eff
 
 foreign import throw ∷ ∀ eff. Eff eff Int
 
-
-data Maybe a = Nothing | Just a
-
--- named args - phils trick
--- f u =
---   let
---     user = u { id: Nothing, email: Nothing, age: Nothing }
---   in
---     user
---
---
--- u = f _{ email = Just "new", age = Just 8 }
-
-
 main :: forall e. Eff (console :: CONSOLE, raven ∷ RAVEN | e) Unit
 main = do
-  r ← raven (Dsn "https://21351561203e42bebb5113a2798b726d@sentry.io/1188784")
+  r ← raven (Dsn "https://179ded71931e4a58b05b892dd837852e@sentry.io/1191644") {x: "Some context"}
+
+  context ← getContext r
+
+  setContext r {x: "New context"}
+
+  traceAnyA "Old context:"
+  traceAnyA context
+
+  newContext ← getContext r
+  traceAnyA "New context:"
+  traceAnyA newContext
+
+  traceAnyA "Old context:"
+  traceAnyA context
+
   captureMessage r ("TEST MESSAGE")
-  -- x ← y r
-  x ← inContext r throw
-  logShow x
+  -- -- x ← y r
+  -- x ← inContext r throw
+  -- logShow x
   -- traceAnyA x
   -- captureMessage r "NO GLOB"
   -- traceAnyA r
