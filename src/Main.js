60eb410ba649a0564f4fd43fcc7602d07205dd80
Michał Adamczyk <michal.anks@gmail.com>
2018-04-19 06:11:29 +0200

    Add context modifying and breadcrumbs recording functions.

--- src/Main.js
+++ src/Main.js
@@ -26,6 +26,38 @@ exports.inContextImpl = function(raven, eff) {
   return raven.context(eff);
 };
 
+exports.extend =
+    Object.assign ||
+    function(target) {
+        for (var i = 1; i < arguments.length; i++) {
+            var source = arguments[i];
+            for (var key in source) {
+                if (Object.prototype.hasOwnProperty.call(source, key)) {
+                    target[key] = source[key];
+                }
+            }
+        }
+        return target;
+    };
+
+
+exports.recordBreadcrumbImpl = function(raven, breadcrumb) {
+    breadcrumb = exports.extend(
+        {
+            timestamp: +new Date() / 1000
+        },
+        breadcrumb
+    );
+    var currCtx = raven.getContext();
+    if (!currCtx.breadcrumbs) currCtx.breadcrumbs = [];
+    currCtx.breadcrumbs.push(breadcrumb);
+    if (currCtx.breadcrumbs.length > raven.maxBreadcrumbs) {
+        currCtx.breadcrumbs.shift();
+    }
+    raven.setContext(currCtx);
+};
+
 exports.throw = function() {
-  return 8;
+    throw new Error("test error");
+    return 8;
 };
--- src/Main.purs
+++ src/Main.purs
@@ -3,11 +3,12 @@ module Main where
 import Prelude
 
 import Control.Monad.Eff (Eff, kind Effect)
-import Control.Monad.Eff.Console (CONSOLE, log, logShow)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Uncurried (EffFn2, runEffFn2, EffFn1, runEffFn1)
 import Debug.Trace (traceAnyA)
-import Unsafe.Coerce (unsafeCoerce)
-
+import Data.Foreign (Foreign)
+import Simple.JSON (class WriteForeign, write)
+-- import Unsafe.Coerce (unsafeCoerce)
 foreign import data Raven ∷ Type → Type
 foreign import data RAVEN ∷ Effect
 
@@ -23,6 +24,8 @@ foreign import getContextImpl ∷ ∀ ctx eff. EffFn1 (raven ∷ RAVEN | eff) (R
 
 foreign import setContextImpl ∷ ∀ ctx eff. EffFn2 (raven ∷ RAVEN | eff) (Raven ctx) ctx Unit
 
+foreign import recordBreadcrumbImpl ∷ ∀ ctx eff. EffFn2 (raven ∷ RAVEN | eff) (Raven ctx) Foreign Unit
+
 raven ∷ ∀ ctx eff. Dsn → ctx → Eff (raven ∷ RAVEN | eff) (Raven ctx)
 raven (Dsn s) ctx = runEffFn2 ravenImpl s ctx
 
@@ -35,21 +38,48 @@ getContext r = runEffFn1 getContextImpl r
 setContext ∷ ∀ ctx eff. Raven ctx → ctx → Eff (raven ∷ RAVEN | eff) Unit
 setContext r ctx = runEffFn2 setContextImpl r ctx
 
+modifyContext ∷ ∀ ctx eff. Raven ctx → (ctx → ctx) → Eff (raven ∷ RAVEN | eff) Unit
+modifyContext r f = (f <$> getContext r) >>= setContext r
+
+withContext ∷ ∀ ctx eff a. Raven ctx → ctx → (Eff (raven ∷ RAVEN | eff) a) → Eff (raven ∷ RAVEN | eff) a
+withContext r ctx = withModifiedContext r (const ctx)
+
+withModifiedContext :: ∀ ctx eff a. Raven ctx → (ctx → ctx) → (Eff (raven ∷ RAVEN | eff) a) → Eff (raven ∷ RAVEN | eff) a
+withModifiedContext r f action = do
+  orig <- getContext r
+  setContext r (f orig)
+  ret <- action
+  setContext r orig
+  pure ret
+
+
+recordBreadcrumb :: ∀ ctx eff bc. WriteForeign bc =>  Raven ctx → bc → Eff (raven ∷ RAVEN | eff) Unit
+recordBreadcrumb r bc = runEffFn2 recordBreadcrumbImpl r (write bc)
+
+
+
+
 -- inContext ∷ ∀ a eff. Raven → Eff (raven ∷ RAVEN | eff) a → Eff (raven ∷ RAVEN | eff) a
 -- inContext r eff = runEffFn2 inContextImpl r eff
 
 foreign import throw ∷ ∀ eff. Eff eff Int
 
-main :: forall e. Eff (console :: CONSOLE, raven ∷ RAVEN | e) Unit
+main ∷ forall e. Eff (console ∷ CONSOLE, raven ∷ RAVEN | e) Unit
 main = do
-  r ← raven (Dsn "https://179ded71931e4a58b05b892dd837852e@sentry.io/1191644") {x: "Some context", breadcrumbs: [{level: "error", category: "test", message: "test message"}]}
+  r ← raven (Dsn "") -- put dsn here
+            {x: "Some context"}
 
   context ← getContext r
 
-  -- setContext r {x: "New context", }
+  setContext r {x: "New context"}
 
   traceAnyA "Old context:"
   traceAnyA context
+
+  withModifiedContext r _{x="Inner"} (do
+    inner ← getContext r
+    traceAnyA "inner context:"
+    traceAnyA inner)
 
   newContext ← getContext r
   traceAnyA "New context:"
@@ -57,12 +87,22 @@ main = do
 
   traceAnyA "Old context:"
   traceAnyA context
+  recordBreadcrumb r {level: "error", category: "test", message: "test message"}
+  captureMessage r ("MESSAGE catch this 3")
 
-  captureMessage r ("MESSAGE")
+  recordBreadcrumb r {level: "warning", category: "test", message: "warning brd"}
+  -- _ ← throw -- uncommenting this line will cause last messages not to be send
+  --           -- as the exception is not hadnled by raven
+
+  recordBreadcrumb r {level: "warning", category: "test", message: "warning brd"}
+
+  captureMessage r ("MESSAGE catch this 4")
   -- -- x ← y r
 
   -- x ← inContext r throw
   -- logShow x
   -- traceAnyA x
   -- captureMessage r "NO GLOB"
+  -- traceAnyA r
+
   -- traceAnyA r
